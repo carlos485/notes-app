b7cca6b9d42efe1f465efd2e456a931e6401dc74
Carlos Pando <carlos.pando-henkcorp@farmaciasperuanas.pe>
2023-12-30 23:11:17 -0500

    feat(button.componente): button component was created

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,11 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
-import { InputComponent } from '../../layout/componentes/input/input.component';
+import { InputComponent } from '../../layout/components/input/input.component';
+import { ButtonComponent } from '../../layout/components/button/button.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [InputComponent],
+  imports: [InputComponent, ButtonComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
