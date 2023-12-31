e522aa3331376c5feb5dea712d71a26050edb870
Carlos Pando <carlos.pando-henkcorp@farmaciasperuanas.pe>
2023-12-30 21:55:02 -0500

    feat(input.componente): the input component was created for the login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { InputComponent } from '../../layout/componentes/input/input.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [InputComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
