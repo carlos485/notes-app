116f70d057a9d7af6f49ddc8373bb384560c8a7a
Carlos Pando <carlospando856@gmail.com>
2024-01-05 16:07:40 -0500

    feat(input.component): from and controlName attributes will be added

--- src/app/layout/components/input/input.component.ts
+++ src/app/layout/components/input/input.component.ts
@@ -1,14 +1,22 @@
 import { NgClass } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, ReactiveFormsModule, FormsModule],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss',
 })
 export class InputComponent {
+  @Input() form: FormGroup = new FormGroup({ field: new FormControl('') });
+  @Input() controlName: string = 'field';
   @Input() type: string | undefined;
   @Input() placeholder: string | undefined;
   @Input() iconLeft: string | undefined;
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,17 +1,32 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { InputComponent } from '../../layout/components/input/input.component';
 import { ButtonComponent } from '../../layout/components/button/button.component';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [InputComponent, ButtonComponent],
+  imports: [InputComponent, ButtonComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   type: string = 'password';
   iconRight: string = 'nt-eye';
+  form: FormGroup;
+
+  constructor(private readonly form_builder: FormBuilder) {
+    this.form = this.form_builder.group({
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
 
   chageType() {
     if (this.type === 'password') {
@@ -21,5 +36,9 @@ export class LoginComponent {
       this.type = 'password';
       this.iconRight = 'nt-eye';
     }
+  }
+
+  login() {
+    console.log(this.form);
   }
 }
