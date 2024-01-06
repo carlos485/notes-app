0fb7ec0dbaf29b5e853f8e85b6864564279650cc
Carlos Pando <carlospando856@gmail.com>
2024-01-05 22:19:52 -0500

    feat(error-message.component): added validations for sample messages

--- src/app/layout/components/error-message/error-message.component.ts
+++ src/app/layout/components/error-message/error-message.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-error-message',
   standalone: true,
   imports: [],
   templateUrl: './error-message.component.html',
-  styleUrl: './error-message.component.scss'
+  styleUrl: './error-message.component.scss',
 })
 export class ErrorMessageComponent {
+  @Input() form: FormGroup = new FormGroup({ field: new FormControl('') });
+  @Input() field: string = 'field';
 
+  get error(): string | undefined {
+    const field = this.form.get(this.field);
+    if (field?.errors && (field?.touched || field?.dirty)) {
+      return Object.keys(this.form.controls[this.field].errors ?? {})[0];
+    }
+    return undefined;
+  }
 }
