0d23604b8f4ab98c1aa7efb1644d23abf6dc1623
Carlos Pando <carlospando856@gmail.com>
2024-01-23 14:31:30 -0500

    feat(form.component.ts): added login service

--- src/app/components/login/form/form.component.ts
+++ src/app/components/login/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -10,6 +10,8 @@ import { InputComponent } from '../../../layout/components/input/input.component
 import { ErrorMessageComponent } from '../../../layout/components/error-message/error-message.component';
 import { ButtonComponent } from '../../../layout/components/button/button.component';
 import { NgClass } from '@angular/common';
+import { AuthService } from '../../../services/auth.service';
+import { ToastService } from '../../../services/toast.service';
 
 @Component({
   selector: 'app-form-login',
@@ -25,7 +27,7 @@ import { NgClass } from '@angular/common';
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
   @Input() register: boolean = false;
   @Input() active: boolean = false;
   @Output() select = new EventEmitter<any>();
@@ -33,12 +35,22 @@ export class FormComponent {
   type: string = 'password';
   iconRight: string = 'nt-eye';
 
-  constructor(private readonly _fb: FormBuilder) {
+  constructor(
+    private readonly _fb: FormBuilder,
+    private readonly auth_service: AuthService,
+    private readonly toast_service: ToastService
+  ) {
     this.form = this._fb.group({
-      name: ['', Validators.required],
+      name: '',
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
     });
+  }
+
+  ngOnInit(): void {
+    if (this.register) {
+      this.form.controls['name'].setValidators(Validators.required);
+    }
   }
 
   chageType(): void {
@@ -52,6 +64,21 @@ export class FormComponent {
   }
 
   submit(): void {
-    console.log(this.form.getRawValue());
+    try {
+      if (this.form.invalid) {
+        this.form.markAllAsTouched();
+      } else {
+        const body = this.form.getRawValue();
+        this.auth_service.login(body).subscribe({
+          next: (e) => console.log(e),
+          error: (err) => {
+            console.log(err.error.message);
+            this.toast_service.showToast('AAAA');
+          },
+        });
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
   }
 }
