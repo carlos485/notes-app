ea4a87660338f5e3a348dd9b6072807751283003
Carlos Pando <carlospando856@gmail.com>
2024-05-05 23:39:22 -0500

    fix: Fix for theme change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}",],
-  darkMode: 'selector',
+  darkMode: 'class',
   theme: {
     extend: {},
   },
