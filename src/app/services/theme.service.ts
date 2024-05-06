73fb6f202ebea8df36b305cc252cfeb4af476aee
Carlos Pando <carlospando856@gmail.com>
2024-05-06 00:19:18 -0500

    feat(theme.service): creating service for theme change

---
+++ src/app/services/theme.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, Renderer2, RendererFactory2 } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+  private themeSubject = new BehaviorSubject<any>(null)
+  themeState$ = this.themeSubject.asObservable()
+  private renderer: Renderer2
+
+  constructor(rendererFactory: RendererFactory2) {
+    this.renderer = rendererFactory.createRenderer(null, null)
+  }
+
+  changeTheme() {
+    const currentTheme = localStorage.getItem('theme')
+    const isDark = currentTheme === 'dark'
+    console.log(isDark)
+    if (!isDark) {
+      this.renderer.addClass(document.documentElement, 'dark')
+      localStorage.setItem('theme', 'dark')
+    } else {
+      this.renderer.removeClass(document.documentElement, 'dark')
+      localStorage.setItem('theme', 'light')
+    }
+  }
+}
