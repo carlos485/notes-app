d9015228ed06bb03a40d9977edf15dcc5a8bb3a8
Carlos Pando <carlospando856@gmail.com>
2024-05-24 00:01:59 -0500

    feat: added plugin flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}",],
-  darkMode: 'class',
+  content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("flowbite/plugin")],
+};
